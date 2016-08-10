2f3b61b84aad051b11004d37f0d5a0cdff663876
acleaud <amaury.cleaud@stimactiv.com>
2016-08-10 17:59:35 +0200

    Ajout du CoreBundle dans kernelApp
    Signed-off-by: acleaud <amaury.cleaud@stimactiv.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new CUI\PagesBundle\CUIPagesBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new CUI\CoreBundle\CUICoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
