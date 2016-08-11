26619a9d3b4ccf1cca168a53f7402a0ee8d0c7eb
acleaud <amaury.cleaud@stimactiv.com>
2016-08-11 09:47:55 +0200

    Factorisation de code du PagesController
    Signed-off-by: acleaud <amaury.cleaud@stimactiv.com>

--- src/CUI/PagesBundle/Controller/PagesController.php
+++ src/CUI/PagesBundle/Controller/PagesController.php
@@ -11,37 +11,10 @@ class PagesController extends Controller
 {
     public function indexAction(Page $page_actuelle)
     {
-
-    	$em = $this->getDoctrine()->getManager();
-
-    	$listeRubriques = $em->getRepository('CUIPagesBundle:Rubrique');
-
-    	$listePages = $em->getRepository('CUIPagesBundle:Page');
-
-    	$menu = $listeRubriques->findAll();
-
-    	foreach($menu as $rub) {
-
-    		$rub->pages =  $listePages->findBy(array("rubrique"=>$rub));
-
-    	}
-
     	if(null === $page_actuelle) {
     		throw new NotFoundHttpException('Page inexistante.');
     	} else {
-    		return $this->render(
-
-    			'CUIPagesBundle:Pages:index.html.twig',
-
-    			array(
-    				'rubrique'=>$page_actuelle->getRubrique()->getNom(),
-    				'nom'=>$page_actuelle->getNom(),
-    				'titre'=>$page_actuelle->getTitre(),
-    				'contenu'=>$page_actuelle->getContenu(),
-    				'menu'=>$menu,
-    				'nbrub'=>count($menu)
-    				)
-    			);
+    		return $this->render('CUIPagesBundle:Pages:index.html.twig', array('page'=>$page_actuelle));
     	}
 
     }
