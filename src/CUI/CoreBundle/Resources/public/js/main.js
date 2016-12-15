78ca30ecb060e272c54f2889681fce6693b2930e
cleaud_ssh <amaury.cleaud@stimactiv.com>
2016-12-15 15:23:01 +0100

    Mise à jour des bannières de pages
    + Les bannières sont découpées en 3 images : le centre en taille fixe et les côtés gauche et droits qui prennent la taille restante
    + Les images de gauche et de droite des bannières sont chargées de manière dynamique en JS
    + Les bannières sont chargées en fonction de l'id de la rubrique dans laquelle la page se trouve. Le nom des images doit donc être bandeau_pages_{id-rubrique}(_gauche/_droite).jpg

--- src/CUI/CoreBundle/Resources/public/js/main.js
+++ src/CUI/CoreBundle/Resources/public/js/main.js
@@ -1,4 +1,14 @@
+function fondBan() {
+	var carousel = $('#carousel');
+	var image1 = carousel.children('.item.active').find('img');
+	var srcImage = image1.attr('src');
+	var newSrcImage1 = srcImage.slice(0,-4)+'_gauche.jpg';
+	var newSrcImage2 = srcImage.slice(0,-4)+'_droit.jpg';
+	carousel.css('background-image', 'url('+newSrcImage1+'), url('+newSrcImage2+')');
+}
+
 $(document).ready(function() {
+	  fondBan();
       $('a[href^="#contact"]').click(function() {
           var target = $(this.hash);
           if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
@@ -7,4 +17,7 @@ $(document).ready(function() {
           $('html, body').animate({ scrollTop: target.offset().top }, 500);
           return false;
       });
+      $('#carousel-principal').on('slide.bs.carousel', function () {
+	  	fondBan();
+	})
   });
